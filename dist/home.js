cb6e18f7b3d57d42b3fcb4b29f73346c9601c968
Deepak <deep84chaudhary@gmail.com>
2023-09-06 16:28:12 +0530

    Added Achiever Section, Story Section, and Footer Section and made responsive these sections

--- dist/home.js
+++ dist/home.js
@@ -1,33 +1,36 @@
-// const searchIcon = document.getElementById("search-icon");
-// const searchContainer = document.getElementById("search-container");
-// const searchBox = document.getElementById("search-box");
-// const searchBoxContainerIcon = document.getElementById("search-icon-container");
-// const searchBoxIcon = document.getElementById("search-box-icon");
-// const hamberger = document.getElementById("hamberger");
+const searchIcon = document.getElementById("search-icon");
+const searchContainer = document.getElementById("search-container");
+const searchBox = document.getElementById("search-box");
+const searchBoxContainerIcon = document.getElementById("search-icon-container");
+const searchBoxIcon = document.getElementById("search-box-icon");
+const hamberger = document.getElementById("hamberger");
+const mainContainer = document.getElementById("main-container");
 
-// // Header Element Show & Hide:
-// searchIcon.addEventListener("click", () => {
-//     searchBox.classList.toggle("hidden");
-//     searchBoxContainerIcon.classList.toggle("hidden");
+// Header Element Show & Hide:
+searchIcon.addEventListener("click", () => {
+    searchBox.classList.toggle("hidden");
+    searchBoxContainerIcon.classList.toggle("hidden");
 
-//     // Toggle the search icon class between fa-magnifying-glass and fa-xmark
-//     if (searchIcon.classList.contains("fa-magnifying-glass")) {
-//         searchIcon.classList.remove("fa-magnifying-glass");
-//         hamberger.classList.remove("fa-bars");
-//         searchIcon.classList.add("fa-xmark");
-//         searchBox.style.position = "fixed";
-//         searchBox.style.top = "6rem";
-//         searchBox.style.left = "1rem";
-//         searchBox.style.width = "95%";
-//         searchBoxIcon.style.position = "fixed";
-//         searchBoxIcon.style.top = "7.5rem";
-//         searchBoxIcon.style.zIndex = "11";
-//     } else {
-//         searchIcon.classList.remove("fa-xmark");
-//         searchIcon.classList.add("fa-magnifying-glass");
-//         hamberger.classList.add("fa-bars");
-//     }
-// });
+    // Toggle the search icon class between fa-magnifying-glass and fa-xmark
+    if (searchIcon.classList.contains("fa-magnifying-glass")) {
+        searchIcon.classList.remove("fa-magnifying-glass");
+        hamberger.classList.remove("fa-bars");
+        searchIcon.classList.add("fa-xmark");
+        searchBox.style.position = "fixed";
+        searchBox.style.top = "6rem";
+        searchBox.style.left = "1rem";
+        searchBox.style.width = "95%";
+        searchBoxIcon.style.position = "fixed";
+        searchBoxIcon.style.top = "7.5rem";
+        searchBoxIcon.style.zIndex = "11";
+        mainContainer.style.display = "none"
+    } else {
+        searchIcon.classList.remove("fa-xmark");
+        searchIcon.classList.add("fa-magnifying-glass");
+        hamberger.classList.add("fa-bars");
+        mainContainer.style.display = "block"
+    }
+});
 
 
 // ----------------- Section - 1 ----------------- //
